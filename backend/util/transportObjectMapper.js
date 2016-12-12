54b137fc45f1e1b882d55a75c433b4725e0638d4
Thomas Weber <weber@openinnovator.net>
2016-12-12 14:47:11 +0100

    Add doc comments for TOMapper

--- backend/util/transportObjectMapper.js
+++ backend/util/transportObjectMapper.js
@@ -1,10 +1,23 @@
+/**
+ * This module exports mapping function to map from e.g. Database Instances
+ * to Transport Objects (TOs) to hide database implementation details, ensure
+ * only the required information is exposed, and calculate potential
+ * additional values.
+ */
+
 /* @see http://momentjs.com */
 const moment = require('moment');
 
+/**
+ * Maps a instance of the Paper database model to a Paper transport object
+ */
 function toPaperTO (paperInstance) {
   return paperInstance;
 }
 
+/**
+ * Maps a instance of the Event database model to a Event transport object
+ */
 function toEventTO (eventInstance) {
   return {
     id: eventInstance.id,
