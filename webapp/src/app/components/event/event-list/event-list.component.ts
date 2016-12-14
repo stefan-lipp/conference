daeca862b02f6bd3c145336348e59ca57fe3558b
MarkusSchnappi <markus.schnappinger@gmx.de>
2016-12-14 14:55:46 +0100

    styling 2

--- webapp/src/app/components/event/event-list/event-list.component.ts
+++ webapp/src/app/components/event/event-list/event-list.component.ts
@@ -33,8 +33,8 @@ export class EventListComponent  {
    
     let dialogRef = this.dialog.open(OverlayComponent, {
        disableClose: true,
+       
     });
-    dialogRef.afterClosed().subscribe( res => console.log(res));
   }
 
 }
