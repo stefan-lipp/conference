481b3c9ed912ec7383e819d31ba0d0c3bd2d40c0
Thomas Weber <weber@openinnovator.net>
2016-12-18 03:01:46 +0100

    Remove unnecessary constructor from overlay

--- webapp/src/app/components/event/event-view/event-view-overlay.component.ts
+++ webapp/src/app/components/event/event-view/event-view-overlay.component.ts
@@ -7,18 +7,13 @@ import { ConferenceEvent } from '../../../models/conference-event.model';
   selector: 'event-view-overlay',
   template: `
    <conference-event-view [event]="event" ></conference-event-view>
-   <p class="btn-wrapper">
+   <p class="align-right">
      <button md-button (click)="dialogRef.close(true)">Back</button>
    </p>
    `,
-   styleUrls: [ './overlay.styles.css' ],
  })
 export class EventViewOverlayComponent {
 
   public event: ConferenceEvent;
-
-  constructor(
-    @Optional() public dialogRef: MdDialogRef<EventViewOverlayComponent>,
-  ) { }
 
 }
