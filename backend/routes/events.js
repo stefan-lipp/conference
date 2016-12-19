d8837e8ec72971ae28838baf9f904202053ae5df
Michael Schreier <schreier@in.tum.de>
2016-12-19 08:36:13 +0100

    moved route

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -63,6 +63,30 @@ function eventSubroutes (app) {
     });
 
   });
+  
+  
+  app.subroute('/favorites', (app) => {
+
+    // GET retrieve list of all events
+    app.get((req, res, next) => {
+      const personId = (req.decoded ? req.decoded.personId : null);
+
+      Event.findAll({
+        include: [
+          { model: Paper },
+          { model: Favorite, where: { personid: personId }, required: true },
+        ]
+      }).then((events) => {
+         console.log(events.map(TOMapper.toEventTO)[0]);
+         res.json(events.map(TOMapper.toEventTO));
+      }).catch(err => res.status(500).json({
+        error: true,
+        success: false,
+        message: 'Unknown error',
+      }));
+    });
+
+  });
 }
 
 module.exports = eventSubroutes;
