30c775ff93239a3ed1a49410eb5e3ed95e80f806
MarkusSchnappi <markus.schnappinger@gmx.de>
2017-01-17 18:30:04 +0100

    Hide personalization features when not logged in
    closes #65

--- webapp/src/app/components/navigation/navigation.component.ts
+++ webapp/src/app/components/navigation/navigation.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services';
 
 @Component({
   selector: 'conference-navigation',
   templateUrl: 'navigation.template.html',
   styleUrls: [ 'navigation.style.scss' ],
 })
-export class NavigationComponent { }
+export class NavigationComponent {
+
+  constructor (
+    public authService: AuthService,
+  ) {}
+
+}
