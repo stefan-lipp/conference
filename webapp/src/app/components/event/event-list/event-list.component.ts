d6f3831df70710f9d1798186b6edee37451e120d
MarkusSchnappi <markus.schnappinger@gmx.de>
2017-01-26 15:56:12 +0100

    Feature/jump back (#90)
    * enable jumps within event-list
    
    * send jump marker when leaving event-view
    
    * add button to jump after reentering
    
    * hide jump buttons while list is loading

--- webapp/src/app/components/event/event-list/event-list.component.ts
+++ webapp/src/app/components/event/event-list/event-list.component.ts
@@ -6,6 +6,7 @@ import {
   Output,
   EventEmitter,
 } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { ConferenceEvent } from '../../../models/conference-event.model';
 import { AuthService } from '../../../services';
@@ -16,6 +17,7 @@ import { MdDialog } from '@angular/material';
 @Component({
   selector: 'conference-event-list',
   templateUrl: 'event-list.template.html',
+  styleUrls: [ 'event-list.styles.scss' ],
 })
 export class EventListComponent  {
 
@@ -26,10 +28,18 @@ export class EventListComponent  {
   @Input()
   public events: ConferenceEvent[] = [ ];
 
+  private lastVisitedLink: string;
+
   constructor (
     private dialog: MdDialog,
+    private router: Router,
     public authService: AuthService,
-  ) { }
+  ) {
+    let url = this.router.url;
+    if (url.includes('#')) {
+      this.lastVisitedLink = url;
+    }
+  }
 
   /**
    * Method for creating the overlay component and passing parameter to it
@@ -47,5 +57,4 @@ export class EventListComponent  {
   public formatTime (time: moment.Moment): string {
     return time.format('MMM Do, HH:mm');
   }
-
 }
