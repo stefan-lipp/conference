32dfa3d464222c130074c005669b1cb7d0818b3b
schrm <michael.schreier1993@gmx.de>
2017-01-30 13:12:41 +0100

    update message documentation

--- webapp/src/app/components/event/event-overview/event-overview.component.ts
+++ webapp/src/app/components/event/event-overview/event-overview.component.ts
@@ -94,7 +94,7 @@ export class EventOverviewComponent implements OnInit {
   }
 
   /**
-   * Sets selectedEvents to all Events from events whoose title contain filterQuery
+   * Sets selectedEvents to all Events from events matchin the filterQuery
    *
    * @return {void}
    */
