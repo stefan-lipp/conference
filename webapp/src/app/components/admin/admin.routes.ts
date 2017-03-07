a7a6a045487ca4a18011524224ae7da35e4f3935
MarkusSchnappi <markus.schnappinger@gmx.de>
2017-03-07 17:22:14 +0100

    Task/admin routes (#120)
    * add admin component
    
    * make admin tools available from menue

--- webapp/src/app/components/admin/admin.routes.ts
+++ webapp/src/app/components/admin/admin.routes.ts
@@ -2,12 +2,17 @@ import { Routes } from '@angular/router';
 
 import { AuthGuard, AdminGuard } from '../../services';
 import { ScheduleEditorComponent } from './schedule';
+import { AdminOverviewComponent } from './overview';
 
 export const ADMIN_ROUTES: Routes = [
   {
     path: 'admin',
     canActivate: [ AuthGuard, AdminGuard ],
     children: [
+      {
+        path: '',
+        component: AdminOverviewComponent,
+      },
       {
         path: 'schedule',
         component: ScheduleEditorComponent,
