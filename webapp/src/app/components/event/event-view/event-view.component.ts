b11d77bf501cc47be836a8a58fbfd1d767de4c8c
MarkusSchnappi <markus.schnappinger@gmx.de>
2017-03-09 13:18:56 +0100

    clean up code

--- webapp/src/app/components/event/event-view/event-view.component.ts
+++ webapp/src/app/components/event/event-view/event-view.component.ts
@@ -21,8 +21,6 @@ export class EventViewComponent implements OnInit {
   @Input()
   public event: ConferenceEvent;
 
-  public showLocation: boolean = false;
-
   constructor (
     private route: ActivatedRoute,
     private eventService: EventService,
@@ -69,31 +67,5 @@ export class EventViewComponent implements OnInit {
       return this.event.startTime.format('HH:mm') + ' - ' + this.event.endTime.format('HH:mm');
     }
     return '- - -';
-  }
-
-  /**
-   *  method to build the graphical location information
-   *
-   * TODO change map with map of VLDB location
-   * TODO get drawing data according to event.room
-   */
-  public drawCanvas (): void {
-    this.showLocation = true;
-
-    const canvas: HTMLCanvasElement = <HTMLCanvasElement> document.getElementById('eventMapCanvas');
-    const context: CanvasRenderingContext2D = canvas.getContext('2d');
-    context.font = '20px Arial';
-    context.fillText('load map', canvas.width / 2, canvas.height / 2);
-
-    // Image set up
-    const img = new Image();
-    img.src = 'http://www.ma.tum.de/foswiki/pub/Mathematik/AnfahrtCampusGarching/fmi.svg';
-    context.drawImage(img, 1, 1, canvas.width , canvas.height);
-
-    // Draw hint
-    const room = [ 100, 80 ]; // Room coordinates
-    const hint = new Image();
-    hint.src = 'https://d30y9cdsu7xlg0.cloudfront.net/png/677417-200.png';
-    context.drawImage(hint, room[0], room[1], 25 , 25);
   }
 }
