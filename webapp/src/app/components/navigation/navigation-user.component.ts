1bad53756404729514bb522c4e9b7e272d8398a7
Thomas Weber <weber@openinnovator.net>
2017-03-14 15:57:12 +0100

    Clean up navigation components
    - Move style from navigation-user.component.ts to global _navigation.scss.
      This was done to not have CSS mixen in with the other code.
    - Replace button of NaivgationComponent with a simple span, since it only
      displays data and is not interactive.
    - Remove some unused code.

--- webapp/src/app/components/navigation/navigation-user.component.ts
+++ webapp/src/app/components/navigation/navigation-user.component.ts
@@ -8,29 +8,11 @@ import { AuthService } from '../../services';
 @Component({
   selector: 'conference-navigation-user',
   template: `
-    <button md-button>
+    <span md-button>
       <i class="material-icons">person_outline</i>
-      <span>{{ username }}</span>
-    </button>
-    `,
-  styles: [ `
-    button {
-      border: thin solid #fff;
-      border-radius: 1em;
-      margin-right: 1em;
-      padding: 0 .6em;
-    }
-
-    span {
-      padding-left: .2em;
-    }
-
-    @media only screen and (max-width: 500px) {
-       span {
-        display: none;
-      }
-    }
-    ` ],
+      <span class="username">{{ username }}</span>
+    </span>
+  `,
 })
 export class NavigationUserComponent implements OnInit {
 
