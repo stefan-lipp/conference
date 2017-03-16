641144c81495eef6629ce151739a221316f80409
Thomas Weber <weber@openinnovator.net>
2017-03-16 15:48:37 +0100

    Fix style issues in event-list.component
    Code-style-wise most notably the camelCase class names.
    The layouting of the card content also used some weired margins and
    paddings to achieve the desired layout.
    This has been replaced with mostly flex-box layout.
    Some of the HTML tags were also replaced with semantically more
    appropriate alternatives.

--- webapp/src/app/components/event/event-list/event-list.component.ts
+++ webapp/src/app/components/event/event-list/event-list.component.ts
@@ -16,10 +16,22 @@ import { AuthService } from '../../../services';
 })
 export class EventListComponent  {
 
+  /**
+   * Event emitter to propagate changes in an events favourite-state
+   *
+   * @type {EventEmitter<[ConferenceEvent, boolean]>}
+   * @memberOf EventListComponent
+   */
   @Output()
   public onFavouriteStateChange: EventEmitter<[ConferenceEvent, boolean]> =
     new EventEmitter<[ConferenceEvent, boolean]>();
 
+  /**
+   * Events to display
+   *
+   * @type {ConferenceEvent[]}
+   * @memberOf EventListComponent
+   */
   @Input()
   public events: ConferenceEvent[] = [ ];
 
